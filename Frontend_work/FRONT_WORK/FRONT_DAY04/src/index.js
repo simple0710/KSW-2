82d5bf37563687fb9747e0fe74f8b65f115c9f7e
simple710 <thesimpleman@naver.com>
2023-02-13 17:19:13 +0900

    2023/02/13

---
+++ Frontend_work/FRONT_WORK/FRONT_DAY04/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./comp_ex/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
