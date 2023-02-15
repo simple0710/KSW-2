69aa00acaea0f3cfa107f7c43d075fa78a1b9a1a
simple710 <112814424+simple0710@users.noreply.github.com>
2023-02-15 19:51:40 +0900

    2023/02/15

--- Frontend_work/FRONT_WORK/FRONT_DAY04/src/comp_ex/App.js
+++ Frontend_work/FRONT_WORK/FRONT_DAY04/src/comp_ex/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+<<<<<<< Updated upstream
 function InputItem({ appendItem }) {
   // input의 value로 사용 할 경우 초기값 필수.
   const [inputWork, setInputWork] = useState("");
@@ -29,12 +30,17 @@ function InputItem({ appendItem }) {
 function ItemRow({ item, removeItem, updateItem }) {
   const [mode, setMode] = useState(false);
   const [title, setTitle] = useState(item.title);
+=======
+function ItemRow({ item, removeItem }) {
+  const [doneCheck, newDoneCheck] = useState(item.done);
+>>>>>>> Stashed changes
   return (
     <li>
       <p>
         <input
           checked={item.done ? "checked" : ""}
           type="checkbox"
+<<<<<<< Updated upstream
           onChange={(e) => {
             item.done = e.target.checked;
             updateItem(item);
@@ -49,6 +55,15 @@ function ItemRow({ item, removeItem, updateItem }) {
             setTitle(e.target.value);
           }}
         />
+=======
+          checked={doneCheck ? true : false}
+          onChange={(e) => {
+            localStorage.getItem(0);
+            newDoneCheck(!doneCheck);
+          }}
+        />
+        <input type="text" style={{textDecoration: doneCheck ? "line-through" : "none"}} value={item.title} disabled />
+>>>>>>> Stashed changes
         <button
           onClick={(e) => {
             setMode(!mode);
@@ -76,7 +91,37 @@ function ItemRow({ item, removeItem, updateItem }) {
 function TodoList({ todoList, removeItem, updateItem }) {
   return (
     <div>
+<<<<<<< Updated upstream
       <ol>
+=======
+      할일 :
+      <input
+        type="text"
+        value={newWork}
+        onChange={(e) => {
+          setNewWork(e.target.value);
+        }}
+      />
+      <button
+        onClick={(e) => {
+          if (newWork) {
+            appendItem(newWork);
+            setNewWork("");
+          }
+        }}
+      >
+        추가
+      </button>
+    </div>
+  );
+}
+
+// Redux를 이용하면 해결된다.
+function TodoList({ todoList, removeItem }) {
+  return (
+    <div>
+      <ul>
+>>>>>>> Stashed changes
         {todoList.map((item, idx) => {
           return (
             <ItemRow
@@ -93,6 +138,7 @@ function TodoList({ todoList, removeItem, updateItem }) {
 }
 
 function App(props) {
+<<<<<<< Updated upstream
   const [todoList, setTodoList] = useState([]);
   const [noCount, setNoCount] = useState(1);
   useEffect(() => {
@@ -104,6 +150,23 @@ function App(props) {
       console.log("data load완료");
     }
   }, []);
+=======
+  // 과제 1 : 취소선 기능 추가
+  // 과제 2 : todoList 데이터를 LocalStorage에 저장
+  const [todoList, setTodoList] = useState([
+    { no: 1, title: "점심 먹기", done: false },
+    { no: 2, title: "산책 하기", done: false },
+    { no: 3, title: "배운 것 복습하기", done: false },
+    { no: 4, title: "내일 수업 예습하기", done: false },
+  ]);
+  // 과제 2
+  todoList.forEach(element => {
+    window.localStorage.setItem(element.no, element.title);
+    console.log(window.localStorage.getItem(element.no));
+
+  });
+  const [noCount, setNoCount] = useState(5);
+>>>>>>> Stashed changes
 
   function saveLocalStorage(newList, noCnt) {
     localStorage.setItem(
@@ -124,9 +187,11 @@ function App(props) {
     var newList = todoList.filter((item, idx) => {
       return item.no != no;
     });
+    window.localStorage.removeItem(no);
     setTodoList(newList);
     saveLocalStorage(newList, noCount);
   }
+<<<<<<< Updated upstream
 
   function updateItem() {
     const newList = [...todoList];
@@ -140,6 +205,13 @@ function App(props) {
         <h1>TodoList</h1>
         <InputItem appendItem={appendItem} />
       </div>
+=======
+  var color = "green";
+  return (
+    <>
+      <h1 style={{color}}>Todo List</h1>
+      <InputItem appendItem={appendItem} />
+>>>>>>> Stashed changes
       <hr />
       <TodoList
         todoList={todoList}
