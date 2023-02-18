11d89f2c02c783a3f2dfcd5c7c4de4e789c3ce48
simple710 <112814424+simple0710@users.noreply.github.com>
2023-02-19 01:27:23 +0900

    2023/02/19

--- FrontEnd_work/Backend_Work/backend_day03_quest/server.js
+++ FrontEnd_work/Backend_Work/backend_day03_quest/server.js
@@ -3,9 +3,13 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
 process.env.PORT = 3000;
 app.set("port", process.env.PORT || 3001);
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
@@ -13,20 +17,39 @@ router.route("/").get((req, res) => {
   res.end();
 });
 
-router.route("/upload").get(function (req, res) {
-  console.log("POST - /get");
-  res.redirect("/");
+infoList = [
+  {no: 1, id: "chan", name: "kim", email: "email@naver.com"},
+];
+
+// infoView.ejs를 보여주는 코드
+router.route("/infoView").get((req, res) => {
+  console.log("GET - /infoView");
+  req.app.render("infoView", { infoList: infoList }, (err, data) => {
+    if (err) throw err;
+    res.end(data);
+  });
 });
 
-router.route("/upload").post((req, res) => {
-  console.log("POST - /upload");
-  res.redirect("/upload");
+
+router.route("infoView/imgSave").post((req, res) => {
+  infoImage = req.body.photo;
+  req.redirect("/infoView");
 });
 
+router.route("/infoView/upload").post((req, res) => {
+  console.log("POST - infoView/upload");
+  console.log(req.body);
+  var newId = req.body.id;
+  var newName = req.body.name;
+  var newEmail = req.body.email;
+  var newPhoto = req.body.photo;
+  infoList.push({no: 1, id: newId, name: newName, email: newEmail, photo: newPhoto});
+  res.redirect("/infoView");
+});
 
 app.use("/", router);
 
 const server = http.createServer(app);
 server.listen(app.get("port"), () => {
   console.log("Node.js 서버 실행 중 ... http://localhost:" + app.get("port"));
-})
+});
