c35e5e83f0c6fdb2fb44d1729f22c3b62d7e4d5c
simple710 <thesimpleman@naver.com>
2023-02-20 09:48:28 +0900

    2023/02/20

--- Frontend_work/Backend_Work/backend_day04/server.js
+++ Frontend_work/Backend_Work/backend_day04/server.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const expressSession = require("express-session");
 // npm i multer -S
 const multer = require("multer");
+const fs = require("fs");
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -25,8 +26,34 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+// post 전송 방식을 사용하기 때문에 bodyParser가 먼저 선언 되어야 한다.
+let storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "uploads");
+  },
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + "_" + file.originalname);
+  },
+});
+// 파일 제한 : 10, 1G이하로 제한.
+let upload = multer({
+  storage: storage,
+  limit: {
+    files: 10,
+    fileSize: 1024 * 1024 * 1024,
+  },
+});
 
 /////// router -------
+router.route("/process/photo").post(upload.array("photo", 1), (req, res) => {
+  console.log("POST - /process/photo 호출 ...");
+  console.log(req.files);
+
+  res.end("file upload!");
+});
+
 router.route("/home").get((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf8" });
   res.write("<h1>길동이의 홈페이지</h1>");
