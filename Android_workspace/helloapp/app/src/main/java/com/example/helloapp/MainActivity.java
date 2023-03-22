0c44daf06063e06fd88f3dab305aa35e8f7d4d1b
simple710 <thesimpleman@naver.com>
2023-03-22 16:48:32 +0900

    2023/03/22

---
+++ Android_workspace/helloapp/app/src/main/java/com/example/helloapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.helloapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
